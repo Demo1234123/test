033944bfc65173df8b5dcac1dfb31ae5b52ac88f
Demo1234123 <105298627+Demo1234123@users.noreply.github.com>
2024-06-23 16:56:31 +0800

    Add files via upload

--- get_real_seq.py
+++ get_real_seq.py
@@ -9,33 +9,34 @@ from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 
 
-def process_tar(ls):
+def process_tar(args):
+    pro, base_dir = args
     # 遍历每一个 .tar 文件
-    for pro in tqdm(ls):
-        if pro.endswith('.tar'):
-            tar_path = os.path.join(base_dir, pro)
+    # for pro in ls:
+    if pro.endswith('.tar'):
+        tar_path = os.path.join(base_dir, pro)
 
-            # 创建一个目录用于解压 .tar 文件
-            extract_dir = os.path.join(base_dir, pro[:-4])
-            os.makedirs(extract_dir, exist_ok=True)
+        # 创建一个目录用于解压 .tar 文件
+        extract_dir = os.path.join(base_dir, pro[:-4])
+        os.makedirs(extract_dir, exist_ok=True)
 
-            # 解压 .tar 文件
-            with tarfile.open(tar_path, "r:") as tar:
-                tar.extractall(path=extract_dir)
+        # 解压 .tar 文件
+        with tarfile.open(tar_path, "r:") as tar:
+            tar.extractall(path=extract_dir)
 
-            # 遍历解压后的目录，查找 .gz 文件并解压
-            for root, dirs, files in os.walk(extract_dir):
-                for file in files:
-                    if file.endswith('.gz'):
-                        gz_path = os.path.join(root, file)
-                        extract_path = os.path.join(root, file[:-3])  # 去掉 .gz 后缀
-                        # 解压 .gz 文件
-                        with gzip.open(gz_path, 'rb') as f_in:
-                            with open(extract_path, 'wb') as f_out:
-                                shutil.copyfileobj(f_in, f_out)
-                        # 可选择删除原始的 .gz 文件
-                        os.remove(gz_path)
-                        get_seq(extract_path)
+        # 遍历解压后的目录，查找 .gz 文件并解压
+        for root, dirs, files in os.walk(extract_dir):
+            for file in files:
+                if file.endswith('.gz'):
+                    gz_path = os.path.join(root, file)
+                    extract_path = os.path.join(root, file[:-3])  # 去掉 .gz 后缀
+                    # 解压 .gz 文件
+                    with gzip.open(gz_path, 'rb') as f_in:
+                        with open(extract_path, 'wb') as f_out:
+                            shutil.copyfileobj(f_in, f_out)
+                    # 可选择删除原始的 .gz 文件
+                    os.remove(gz_path)
+                    get_seq(extract_path)
 
 
 def get_seq(seq_pa):
@@ -56,8 +57,9 @@ def get_seq(seq_pa):
 
 if __name__ == '__main__':
     # 目标目录
-    base_dir = 'spire'
+    base_dir = 'down'
     # 列出目录中的所有文件
     protein_ls = os.listdir(base_dir)
     with Pool(cpu_count()) as pool:
-        list(tqdm(pool.imap(process_tar, protein_ls), total=len(protein_ls)))
+        args = [(pro, base_dir) for pro in protein_ls]
+        list(tqdm(pool.imap(process_tar, args), total=len(protein_ls)))
